f7d02b095bd11eca06670901eb314d9be9ad4f81
Romario Gustave <romagustave10@gmail.com>
2020-07-07 20:39:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,8 +5,15 @@ class App < Sinatra::Base
     erb :newteam
   end
 
-  post '/team' do
-    puts params
+ post '/team' do
+    @name = params["name"]
+    @coach = params["coach"]
+    @pg = params["pg"]
+    @sg = params["sg"]
+    @sf = params["sf"]
+    @pf = params["pf"]
+    @center = params["c"]
+
     erb :team
   end
 end
