23f20890216df9d01824f35e50d4372175bbbc37
Romario Gustave <romagustave10@gmail.com>
2020-07-07 20:50:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,3 @@
-require 'sinatra/base'
-
 class App < Sinatra::Base
 
   get '/newteam' do
@@ -7,14 +5,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @name = params["name"]
-    @coach = params["coach"]
-    @pg = params["pg"]
-    @sg = params["sg"]
-    @sf = params["sf"]
-    @pf = params["pf"]
-    @center = params["c"]
-
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @pf = params[:pf]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @c = params[:c]
     erb :team
   end
 
